975897f5dd7e02cd5dcc5033b9178a552841444b
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-12-28 04:57:54 +0600

    add products API with MongoDB connection and CORS setup

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 8000;
 
-
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.kybpity.mongodb.net/?appName=Cluster0";
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kybpity.mongodb.net/?appName=Cluster0`;
 
@@ -22,11 +22,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
       console.log("ZaheenKintwear successfully connected to MongoDB!");
       
+    // DB Collections
+      // create database for products collection  
+      const productsCollection = client.db('zaheenkintwear').collection('products')
+
+
+    // Get all products
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
 
 
       
@@ -41,9 +51,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Sir!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`zaheenkintwear app listening on port ${port}`)
 })
